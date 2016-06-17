667135cda7d535ddd2b560c836c3a9059206d679
venky97 <venkateshwaran12345@gmail.com>
2016-06-17 17:56:11 +0530

    Add files via upload

---
+++ pj2.js
@@ -0,0 +1,87 @@
+(function () {
+			
+	var coin,coinImage,canvas;					
+
+	function gameLoop () {
+	
+	  window.requestAnimationFrame(gameLoop);
+
+	  coin.update();
+	  coin.render();
+	}
+	
+	function sprite (options) {
+	
+		var that = {},
+			frameIndex = 0,
+			tickCount = 0,
+			ticksPerFrame = options.ticksPerFrame || 0,
+			numberOfFrames = options.numberOfFrames || 1;
+		
+		that.context = options.context;
+		that.width = options.width;
+		that.height = options.height;
+		that.image = options.image;
+		
+		that.update = function () {
+
+            tickCount += 1;
+
+            if (tickCount > ticksPerFrame) {
+
+				tickCount = 0;
+				
+                // If the current frame index is in range
+                if (frameIndex < numberOfFrames - 1) {	
+                    // Go to the next frame
+                    frameIndex += 1;
+                } else {
+                    frameIndex = 0;
+                }
+            }
+        };
+		
+		that.render = function () {
+		
+		  // Clear the canvas
+		  that.context.clearRect(0, 0, that.width, that.height);
+		  
+		  // Draw the animation
+		  that.context.drawImage(
+		    that.image,
+		    frameIndex * that.width / numberOfFrames,
+		    0,
+		    that.width / numberOfFrames,
+		    that.height,
+		    0,
+		    0,
+		    that.width / numberOfFrames,
+		    that.height);
+		};
+		
+		return that;
+	}
+	
+	// Get canvas
+	canvas = document.getElementById("Animation");
+	canvas.width = 100;
+	canvas.height = 100;
+	
+	// Create sprite sheet
+	coinImage = new Image();	
+	
+	// Create sprite
+	coin = sprite({
+		context: canvas.getContext("2d"),
+		width: 288,
+		height: 110,
+		image: coinImage,
+		numberOfFrames: 4,
+		ticksPerFrame: 10
+	});
+	
+	// Load sprite sheet
+	coinImage.addEventListener("load", gameLoop);
+	coinImage.src = "c.gif";
+
+} ());
